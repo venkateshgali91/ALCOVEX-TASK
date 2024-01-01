ff8b7de99d1411a5bd48b3e3256d0aa1f6787f14
venkateshgali91 <87367976+venkateshgali91@users.noreply.github.com>
2024-01-01 21:12:59 +0530

    Add files via upload

---
+++ dist/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { TaskBoardsWith } from "./screens/TaskBoardsWith";
+
+const app = document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+root.render(<TaskBoardsWith />);
+
+
+
+
